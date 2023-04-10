4df13d502af4da5b1c342148d7946439296dd511
Nicolas <nicolashubert07@hotmail.com>
2023-04-10 19:25:44 +0200

    refactoring

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup, ResultSet
 import os
 
 
-def anti_captcha(url, cookie, siteKey):
+def anti_captcha(url, cookie, site_key):
     solver = turnstileProxyless()
     solver.set_verbose(0)
     solver.set_key(get_anticaptcha_api_key())
     solver.set_website_url(url)
     solver.set_cookies("PHPSESSID=" + cookie)
-    solver.set_website_key(siteKey)
+    solver.set_website_key(site_key)
 
     solver.set_soft_id(0)
 
@@ -27,15 +27,15 @@ def get_anticaptcha_api_key():
 
 
 def get_protected_link(url, token):
-    jsonObject = {'subform': 'unlock', 'cf-turnstile-response': token}
-    response = requests.post(url, data=jsonObject)
-    htmlSoup = BeautifulSoup(response.text, "html.parser")
-    urls = htmlSoup.find(class_="urls")
+    json_object = {'subform': 'unlock', 'cf-turnstile-response': token}
+    response = requests.post(url, data=json_object)
+    html = get_html(response.text)
+    urls = html.find(class_="urls")
     return urls.find("a").attrs.get("href")
 
 
-def get_cookie(firstPageResponse):
-    return firstPageResponse.cookies.get("PHPSESSID")
+def get_cookie(first_page_response):
+    return first_page_response.cookies.get("PHPSESSID")
 
 
 def get_first_page_response(url):
@@ -51,40 +51,40 @@ def get_dl_protect_link(url):
     return get_protected_link(url, token)
 
 
-def get_one_fichier_links(postInfoChildren: ResultSet):
-    alreadySet = False
+def get_one_fichier_links(post_info_children: ResultSet):
+    already_set = False
     links = []
 
-    for tag in postInfoChildren:
-        if (tag.findNext().name == 'div'):
-            if (tag.text.__contains__("1fichier")):
-                alreadySet = True
-            elif alreadySet:
+    for tag in post_info_children:
+        if tag.findNext().name == 'div':
+            if tag.text.__contains__("1fichier"):
+                already_set = True
+            elif already_set:
                 break
-        elif tag.findNext().name == 'a' and alreadySet:
+        elif tag.findNext().name == 'a' and already_set:
             links.append(tag.findNext().attrs.get("href"))
     return links
 
 
 def get_dl_protect_links(url):
     response = requests.get(url)
-    htmlResponse = get_html(response.text)
-    postInfo = htmlResponse.find(class_="postinfo")
-    return get_one_fichier_links(postInfo.findAll('b'))
+    html_response = get_html(response.text)
+    post_info = html_response.find(class_="postinfo")
+    return get_one_fichier_links(post_info.findAll('b'))
 
 
-def get_html(htmlText):
-    return BeautifulSoup(htmlText, "html.parser")
+def get_html(html_text):
+    return BeautifulSoup(html_text, "html.parser")
 
 
 def find_links(url):
-    dlProtectLinks = get_dl_protect_links(url)
-    protectedLinks = []
-    for link in dlProtectLinks:
-        protectedLink = get_dl_protect_link(link)
-        print(protectedLink)
-        protectedLinks.append(protectedLink)
-    return "\n".join(protectedLinks)
+    dl_protect_links = get_dl_protect_links(url)
+    protected_links = []
+    for link in dl_protect_links:
+        protected_link = get_dl_protect_link(link)
+        print(protected_link)
+        protected_links.append(protected_link)
+    return "\n".join(protected_links)
 
 
 if __name__ == '__main__':
